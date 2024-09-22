22dfc9621db92c2ab0a3c76020a7b23dd6a69cae
Lithopsian <deadbeef@nartowicz.co.uk>
2024-09-22 13:24:14 +0100

    Version bump
    1.0!

--- src/Version.h
+++ src/Version.h
@@ -11,10 +11,10 @@
 #define Version_h
 
 #define QDIRSTAT_APP "Qt6DirStat"
-#define QDIRSTAT_VERSION "0.4"
+#define QDIRSTAT_VERSION "1.0"
 #define MENUBAR_VERSION "V" QDIRSTAT_VERSION
 
-#define RELEASE_VERSION  "0.4"
+#define RELEASE_VERSION  "1.0"
 #define RELEASE_BASE_URL "https://github.com/Lithopsian/qt6dirstat/releases/tag/"
 #define RELEASE_URL      RELEASE_BASE_URL RELEASE_VERSION
 
